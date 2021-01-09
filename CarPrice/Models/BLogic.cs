3194f6678fbc8043a9249238e2e21741d7f7fc8a
Maxumka <maxumka1792@gmail.com>
2021-01-09 19:10:38 +0700

    Добавьте файлы проекта.

---
+++ CarPrice/Models/BLogic.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CarPrice.Models
+{
+    static public class BLogic
+    {
+        public static string CalcCostCar(Car car) => car switch
+        {
+            Car when car.Transmission => "High price :)",
+            _ => "Low price :("
+        };
+    }
+}
---
+++ CarPrice/ViewModels/MainViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using CarPrice.Models;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using static CarPrice.Models.BLogic;
+
+namespace CarPrice.ViewModels
+{
+    public sealed class MainViewModel : INotifyPropertyChanged
+    {
+        private Car curCar;
+        public Car CurCar
+        {
+            get => curCar;
+            set { curCar = value; OnPropertyChanged("Car"); }
+        }
+
+        private string test;
+        public string Test
+        {
+            get => test;
+            set { test = value; OnPropertyChanged("Test"); }
+        }
+
+        public MainViewModel()
+        {
+            CurCar = new();
+        }
+
+        private Command calcCommand;
+        public Command CalcCommand
+        {
+            get => calcCommand ??= new Command(obj => { Test = CalcCostCar(obj as Car).ToString(); });
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "") => PropertyChanged?.Invoke(this, new(prop));
+    }
+}
