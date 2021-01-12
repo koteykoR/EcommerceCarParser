03f9e06bf16ba161edc52a440f82eca38becec9a
Maxumka <maxumka1792@gmail.com>
2021-01-12 17:40:57 +0700

    Add prop int Price in Car

--- CarPrice/Models/Car.cs
+++ CarPrice/Models/Car.cs
@@ -1,10 +1,11 @@
 ﻿using System;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
+
 
 namespace CarPrice.Models
 {
-    internal sealed record Car(string Company, string Model, int Mileage, int EnginePower, int EngineVolume, string Year, bool Transmission)
+    internal sealed record Car(string Company, string Model, int Mileage, 
+                               int EnginePower, int EngineVolume, string Year, 
+                               bool Transmission, int Price = 0)
     {
         public Car() : this(default, default, default, default, default, "2020", true) { }
     }
